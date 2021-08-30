0b2fa1077e354d59f00486d4ddb8dcb1bdcd83c0
Gabriel Cerutti <ceruttigabriel@gmail.com>
2021-08-30 23:36:31 +0200

    Updated home page

--- src/microfrontend.tsx
+++ src/microfrontend.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { eventsToDispatch } from './events';
 
 export const render = (containerId: string, history: any, data: any) => {
@@ -9,9 +9,9 @@ export const render = (containerId: string, history: any, data: any) => {
   if (!container) return;
   ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App {...data} />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
     container
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import ChipDemo from '../components/ChipDemo';
 import CardDemo from '../components/CardDemo';
 import PaperDemo from '../components/PaperDemo';
 import ButtonDemo from '../components/ButtonDemo';
@@ -16,15 +14,14 @@ const HomePage = (props: { basePath: string | undefined }) => {
   const classes = useStyles();
 
   return (
-    <Container fixed>
+    <>
       <Typography className={classes.textLeft} variant="h4" component="h3">
         Material UI Demo
       </Typography>
-      <ChipDemo></ChipDemo>
       <CardDemo basePath={props.basePath}></CardDemo>
       <ButtonDemo></ButtonDemo>
       <PaperDemo></PaperDemo>
-    </Container>
+    </>
   );
 };
 
