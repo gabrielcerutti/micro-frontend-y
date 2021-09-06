83398cc3a1663127556649079fc6ffb2e535ba37
Gabriel Cerutti <ceruttigabriel@gmail.com>
2021-09-06 23:02:51 +0200

    Modified approach to setup the microfrontend (not using react-app-rewired)

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,18 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
   webpack: (config) => {
-    config.optimization.runtimeChunk = false;
+    config.optimization.runtimeChunk = true;
     config.optimization.splitChunks = {
       cacheGroups: {
         default: false,
       },
     };
-    config.entry = {
-      main: [path.resolve('.', 'src', 'microfrontend.tsx')],
-    };
-    config.output.library = 'MicroAppY';
-    config.output.libraryTarget = 'window';
+    // config.entry = {
+    //   main: [path.resolve('.', 'src', 'microfrontend.tsx')],
+    // };
+    // config.output.library = 'MicroAppY';
+    // config.output.libraryTarget = 'window';
     return config;
   },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { MicrofrontendOptions } from './microfrontend';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      {/* Pass an empty basePath if you are not using GitHub Pages and just localhost as a base URL */}
-      <App basePath="/micro-frontend-y" host="" />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* 
+  This is commented out because this micro-frontend is following a different approach.
+  It's not using react-app-rewired to build the app, so the entry point is the regular entry point for a react app.
+  That's why we have to comment this piece of code and add global functions to be able to load the app into the Main SPA
+*/
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       {/* Pass an empty basePath if you are not using GitHub Pages and just localhost as a base URL */}
+//       <App basePath="/micro-frontend-y" host="" />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+declare global {
+  interface Window {
+    renderMicroAppY: (containerId: string, options: MicrofrontendOptions) => void;
+  }
+  interface Window {
+    unMountMicroAppY: (containerId: string) => void;
+  }
+}
+
+window.renderMicroAppY = (containerId: string, options: MicrofrontendOptions) => {
+  const container = document.getElementById(containerId);
+  if (!container) return;
+  ReactDOM.render(
+    <React.StrictMode>
+      {/* HashRouter is used here in replacement of BrowserRouter because GitHub doesn't support browser history */}
+      <HashRouter>
+        <App basePath={options.basePath} host={options.host} {...options.data} />
+      </HashRouter>
+    </React.StrictMode>,
+    container
+  );
+  console.log(`Micro-frontend ${containerId} mounted`);
+};
+
+window.unMountMicroAppY = (containerId: string) => {
+  const container = document.getElementById(containerId);
+  if (!container) return;
+  ReactDOM.unmountComponentAtNode(container);
+  console.log(`Micro-frontend ${containerId} unmounted`);
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
